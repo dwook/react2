19d90d371589d282c643c7bf7465c3a51a5b9110
dwook <igerapex@gmail.com>
2017-12-05 21:43:18 +0900

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {  // React.Component를 상속받음.
+    render() {
+
+        return (
+            <h1>Hello World</h1>
+        );
+    }
+}
+
+export default App;  // App으로 도출
